737494d3c37fa42eb0e8654e906a4cabf15ed881
DorAlter <dor.alter@ru.nl>
2023-09-24 22:50:47 +0200

    Fix

--- src/simd/xoodoo.rs
+++ src/simd/xoodoo.rs
@@ -7,7 +7,7 @@ use crunchy::unroll;
 const MAX_ROUNDS: usize = 12;
 
 /// Xoodoo\[n_r\] round constants, taken from table 2 of https://ia.cr/2018/767
-const RC: [usize; MAX_ROUNDS] = [
+const RC: [u32; MAX_ROUNDS] = [
     0x00000058, 0x00000038, 0x000003c0, 0x000000d0, 0x00000120, 0x00000014, 0x00000060, 0x0000002c,
     0x00000380, 0x000000f0, 0x000001a0, 0x00000012,
 ];
@@ -213,7 +213,7 @@ where
 
     let mut tmp = [Simd::<u32, N>::splat(0u32); 12];;
 
-    tmp[0] = e[0] ^ state[0] ^ Simd::<usize, N>::splat(round_key);
+    tmp[0] = e[0] ^ state[0] ^ Simd::<u32, N>::splat(round_key);
     tmp[1] = e[1] ^ state[1];
     tmp[2] = e[2] ^ state[2];
     tmp[3] = e[3] ^ state[3];
